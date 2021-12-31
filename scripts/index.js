abf25b5eae1cb8b6a4af28c644dd5028433f922a
mikeHerceg <mikehercegdesigns@gmail.com>
2021-12-31 10:57:33 -0500

    making code object oriented

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,13 @@
 
-
-    const target = document.getElementById('move-with-mouse')
-    const dataSection = document.getElementById('data')
-    const targetRect = target.getBoundingClientRect()
-    let listenerState
+function mouseEffect(targetId) {
+    this.target = document.getElementById(targetId)
+    this.dataSection = document.getElementById('data')
+    this.listenerState = false
     const getData = (e) =>{
         const data = {
-            target:target.offsetTop,
-            targetCenterX: target.offsetLeft + (target.offsetWidth / 2),
-            targetCenterY: target.offsetTop + (target.offsetHeight / 2),
+            target:this.target.offsetTop,
+            targetCenterX: this.target.offsetLeft + (this.target.offsetWidth / 2),
+            targetCenterY: this.target.offsetTop + (this.target.offsetHeight / 2),
             mouseX:e.clientX,
             mouseY:e.clientY
         }
@@ -17,10 +16,8 @@
     // use to display data being tracked
     const setData = (data) => {
         const {mouseX, mouseY, targetCenterX, targetCenterY, window, target} = data
-        dataSection.innerText = 
+        this.dataSection.innerText = 
         `
-        target: ${targetRect} \n
-
         mouse location: ${mouseX}, ${mouseY} \n
         targetDimensions: ${targetCenterX}, ${targetCenterY}
         `
@@ -30,7 +27,7 @@
         const {mouseX, mouseY, targetCenterX, targetCenterY} = data; 
         let x = (mouseX - targetCenterX) / 50
         let y = (mouseY - targetCenterY) / 50
-        target.style = `--x:${x}deg; --y:${y}deg;`
+        this.target.style = `--x:${x}deg; --y:${y}deg;`
         console.log('x:'+x+', y:'+y)
     }
     const watchmouse = (e) => {
@@ -39,27 +36,32 @@
         rotateBox(data);
     }
     const cleanUp = () =>{
-        target.style=null
+        this.target.style=null
     }
- 
+     
     
-const watchPostion = () =>{
-    const targetRect = target.getBoundingClientRect()   
-    const windowHieght =  window.innerHeight
-    if( targetRect.y >= -1 && windowHieght >= targetRect.y){
-        if(listenerState) return
-        listenerState = true
-        window.addEventListener('mousemove',watchmouse);
-        window.addEventListener('mouseout', cleanUp)
-    }else{
-        listenerState = false
-        window.removeEventListener('mousemove', watchmouse);
-        window.removeEventListener('mouseout', cleanUp)
-
+    const watchPostion = () =>{
+        const targetRect = this.target.getBoundingClientRect()   
+        const windowHieght =  window.innerHeight
+        if( targetRect.y >= -1 && windowHieght >= targetRect.y){
+            if(this.listenerState) return
+            this.listenerState = true
+            window.addEventListener('mousemove',watchmouse);
+            window.addEventListener('mouseout', cleanUp)
+        }else{
+            this.listenerState = false
+            window.removeEventListener('mousemove', watchmouse);
+            window.removeEventListener('mouseout', cleanUp)
+        }
+    
     }
-
+    
+    this.intilize = window.addEventListener('scroll', watchPostion)
+    
 }
 
-window.addEventListener('scroll', watchPostion)
+
+const main = new mouseEffect('move-with-mouse')
+main.intilize
 
 
